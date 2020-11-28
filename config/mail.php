212e2914ddbb277dbe1abdeab849189599ab359b
Aries Vincent Dajay <avdajay@gmail.com>
2020-11-28 17:39:11 +0800

    Changed credentials on mail configuration

--- config/mail.php
+++ config/mail.php
@@ -3,9 +3,9 @@
 return [
     'host'     => 'smtp.gmail.com',
     'port'     => '587',
-    'username' => 'jobswander@gmail.com',
-    'password' => 'CyJa@2020',
-    'email'    => 'jobswander@gmail.com',
+    'username' => 'you@email.com',
+    'password' => 'somepassword',
+    'email'    => 'you@email.com',
     'name'     => 'Jobs4You System',
     'encryption' => 'tls'
 ];
